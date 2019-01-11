c083a6fb4f30bdbcf49a03b54b99626e86f75e10
farahamawi <f.amawi@i-q.com>
2019-01-11 20:43:34 +0200

    Test production env

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-const Config = require('config');
+// const Config = require('config');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
 // connect to mongo collection "pirates"
-const mongoConfig = Config.get('mongodb');
-const mongodbURI = `mongodb://${mongoConfig.username}:${mongoConfig.password}@${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.name}`;
-const mongoURI = process.env.MONGODB_URI || mongodbURI;
+// const mongoConfig = Config.get('mongodb');
+// const mongodbURI = `mongodb://${mongoConfig.username}:${mongoConfig.password}@${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.name}`;
+const mongoURI = process.env.MONGODB_URI;
 mongoose.connect(mongoURI, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 
